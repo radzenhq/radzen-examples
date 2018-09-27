4a7a1eba69d1a4e7045ea9ba56c3059e16d07bf4
Vladimir Enchev <vladimir.enchev@gmail.com>
2018-09-27 10:54:59 +0300

    HierarchyWithTwoDataGrid sample added

---
+++ HierarchyWithTwoDataGrid/server/Controllers/Northwind/OrdersController.cs
@@ -0,0 +1,226 @@
+using System;
+using System.Net;
+using System.Data;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.AspNet.OData.Query;
+
+
+
+namespace HierarchyWithTwoDataGrid.Controllers.Northwind
+{
+  using Models;
+  using Data;
+  using Models.Northwind;
+
+  [ODataRoutePrefix("odata/Northwind/Orders")]
+  [Route("mvc/odata/Northwind/Orders")]
+  public partial class OrdersController : ODataController
+  {
+    private Data.NorthwindContext context;
+
+    public OrdersController(Data.NorthwindContext context)
+    {
+      this.context = context;
+    }
+    // GET /odata/Northwind/Orders
+    [EnableQuery(MaxExpansionDepth=10)]
+    [HttpGet]
+    public IEnumerable<Models.Northwind.Order> GetOrders()
+    {
+      var items = this.context.Orders.AsQueryable<Models.Northwind.Order>();
+
+      this.OnOrdersRead(ref items);
+
+      return items;
+    }
+
+    partial void OnOrdersRead(ref IQueryable<Models.Northwind.Order> items);
+
+    [EnableQuery(MaxExpansionDepth=10)]
+    [HttpGet("{OrderID}")]
+    public SingleResult<Order> GetOrder(int key)
+    {
+        var items = this.context.Orders.Where(i=>i.OrderID == key);
+
+        return SingleResult.Create(items);
+    }
+    partial void OnOrderDeleted(Models.Northwind.Order item);
+
+    [HttpDelete("{OrderID}")]
+    public IActionResult DeleteOrder(int key) 
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            var item = this.context.Orders
+                .Where(i => i.OrderID == key)
+                .Include(i => i.OrderDetails)
+                .FirstOrDefault();
+
+            if (item == null)
+            {
+                return BadRequest();
+            }                
+
+            this.OnOrderDeleted(item);
+            this.context.Orders.Remove(item);
+            this.context.SaveChanges();
+
+            return new NoContentResult();
+        }
+        catch(Exception ex) 
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+
+    partial void OnOrderUpdated(Models.Northwind.Order item);
+
+    [HttpPut("{OrderID}")]
+    public IActionResult PutOrder(int key, [FromBody]Models.Northwind.Order newItem)
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (newItem == null || (newItem.OrderID != key))
+            {
+                return BadRequest();
+            }
+
+            this.OnOrderUpdated(newItem);
+            this.context.Orders.Update(newItem);
+            this.context.SaveChanges();
+
+            var itemToReturn = this.context.Orders
+                .Where(i => i.OrderID == key)
+                .Include(i => i.Customer)
+                .Include(i => i.Employee)
+                .Include(i => i.Shipper)
+                .FirstOrDefault();
+
+            return new JsonResult(itemToReturn, new Newtonsoft.Json.JsonSerializerSettings
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
+                DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc
+            })
+            {
+                StatusCode = 200
+            };
+        }
+        catch(Exception ex) 
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+
+    [HttpPatch("{OrderID}")]
+    public IActionResult PatchOrder(int key, [FromBody]Delta<Models.Northwind.Order> patch)
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = this.context.Orders.Where(i=>i.OrderID == key).FirstOrDefault();
+            
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            patch.Patch(item);
+
+            this.OnOrderUpdated(item);
+            this.context.Orders.Update(item);
+            this.context.SaveChanges();
+
+            var itemToReturn = this.context.Orders
+                .Where(i => i.OrderID == key)
+                .Include(i => i.Customer)
+                .Include(i => i.Employee)
+                .Include(i => i.Shipper)
+                .FirstOrDefault();
+
+            return new JsonResult(itemToReturn, new Newtonsoft.Json.JsonSerializerSettings
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
+                DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc
+            })
+            {
+                StatusCode = 200
+            };
+        }
+        catch(Exception ex) 
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+
+    partial void OnOrderCreated(Models.Northwind.Order item);
+
+    [HttpPost]
+    public IActionResult Post([FromBody] Models.Northwind.Order item)
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            this.OnOrderCreated(item);
+            this.context.Orders.Add(item);
+            this.context.SaveChanges();
+
+            var key = item.OrderID;
+            var itemToReturn = this.context.Orders
+                .Where(i => i.OrderID == key)
+                .Include(i => i.Customer)
+                .Include(i => i.Employee)
+                .Include(i => i.Shipper)
+                .FirstOrDefault();
+
+            return new JsonResult(itemToReturn, new Newtonsoft.Json.JsonSerializerSettings
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
+                DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc
+            })
+            {
+                StatusCode = 201
+            };
+        }
+        catch(Exception ex) 
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+  }
+}
---
+++ HierarchyWithTwoDataGrid/server/Startup.cs
@@ -0,0 +1,163 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+
+using HierarchyWithTwoDataGrid.Data;
+
+namespace HierarchyWithTwoDataGrid
+{
+  public partial class Startup
+  {
+    public Startup(IConfiguration configuration, IHostingEnvironment env)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    partial void OnConfigureServices(IServiceCollection services);
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddOptions();
+      services.AddCors();
+
+      services.AddMvc(options =>
+      {
+          options.FormatterMappings.SetMediaTypeMappingForFormat("csv", "text/csv");
+          options.OutputFormatters.Add(new HierarchyWithTwoDataGrid.Data.CsvDataContractSerializerOutputFormatter());
+          options.OutputFormatters.Add(new HierarchyWithTwoDataGrid.Data.XlsDataContractSerializerOutputFormatter());
+      });
+
+      services.AddAuthorization();
+      services.AddOData();
+      services.AddODataQueryFilter();
+
+
+      services.AddDbContext<HierarchyWithTwoDataGrid.Data.NorthwindContext>(options =>
+      {
+        options.UseSqlServer(Configuration.GetConnectionString("NorthwindConnection"));
+      });
+
+      OnConfigureServices(services);
+    }
+
+    partial void OnConfigure(IApplicationBuilder app);
+    partial void OnConfigureOData(ODataConventionModelBuilder builder);
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+      loggerFactory.AddDebug();
+
+      IServiceProvider provider = app.ApplicationServices.GetRequiredService<IServiceProvider>();
+
+      app.UseCors(builder =>
+        builder.WithOrigins("*")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials()
+               .AllowAnyOrigin()
+      );
+
+      app.Use(async (context, next) => {
+          try
+          {
+              await next();
+          }
+#pragma warning disable 0168
+          catch (Microsoft.AspNetCore.Mvc.Internal.AmbiguousActionException ex) {
+#pragma warning restore 0168
+              if (!Path.HasExtension(context.Request.Path.Value)) {
+                  context.Request.Path = "/index.html";
+                  await next();
+              }
+          }
+
+          if ((context.Response.StatusCode == 404 || context.Response.StatusCode == 401) && !Path.HasExtension(context.Request.Path.Value)) {
+              context.Request.Path = "/index.html";
+              await next();
+          }
+      });
+
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+
+      app.UseDeveloperExceptionPage();
+
+      app.UseMvc(builder =>
+      {
+          builder.Count().Filter().OrderBy().Expand().Select().MaxTop(null).SetTimeZoneInfo(TimeZoneInfo.Utc);
+
+          if (env.EnvironmentName == "Development")
+          {
+              builder.MapRoute(name: "default",
+                template: "{controller}/{action}/{id?}",
+                defaults: new { controller = "Home", action = "Index" }
+              );
+          }
+
+          var oDataBuilder = new ODataConventionModelBuilder(provider);
+
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Category>("Categories");
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Customer>("Customers");
+
+          var customerCustomerDemo = oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.CustomerCustomerDemo>("CustomerCustomerDemos");
+          customerCustomerDemo.EntityType.HasKey(entity => new {
+            entity.CustomerID, entity.CustomerTypeID
+          });
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.CustomerDemographic>("CustomerDemographics");
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Employee>("Employees");
+
+          var employeeTerritory = oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.EmployeeTerritory>("EmployeeTerritories");
+          employeeTerritory.EntityType.HasKey(entity => new {
+            entity.EmployeeID, entity.TerritoryID
+          });
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Order>("Orders");
+
+          var orderDetail = oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.OrderDetail>("OrderDetails");
+          orderDetail.EntityType.HasKey(entity => new {
+            entity.OrderID, entity.ProductID
+          });
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Product>("Products");
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Region>("Regions");
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Shipper>("Shippers");
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Supplier>("Suppliers");
+          oDataBuilder.EntitySet<HierarchyWithTwoDataGrid.Models.Northwind.Territory>("Territories");
+
+          this.OnConfigureOData(oDataBuilder);
+
+          var model = oDataBuilder.GetEdmModel();
+
+          builder.MapODataServiceRoute("odata/Northwind", "odata/Northwind", model);
+
+      });
+
+
+      if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("RADZEN")) && env.IsDevelopment())
+      {
+        app.UseSpa(spa =>
+        {
+          spa.Options.SourcePath = "../client";
+          spa.UseAngularCliServer(npmScript: "start -- --port 8000 --open");
+        });
+      }
+
+      OnConfigure(app);
+    }
+  }
+}
