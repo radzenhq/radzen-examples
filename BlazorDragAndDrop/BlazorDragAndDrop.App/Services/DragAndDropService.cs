82b6404535caaab1274952bc684576a3492f6dbd
Atanas Korchev <akorchev@gmail.com>
2018-11-13 18:27:30 +0200

    Add BlazorDragAndDrop sample application.

---
+++ BlazorDragAndDrop/BlazorDragAndDrop.App/Services/DragAndDropService.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace BlazorDragAndDrop.App.Services
+{
+    public class DragAndDropService
+    {
+        public object Data {get; set;}
+        public string Zone {get; set;}
+        public void StartDrag(object data, string zone)
+        {
+            this.Data = data;
+            this.Zone = zone;
+        }
+
+        public bool Accepts(string zone)
+        {
+            return this.Zone == zone;
+        }
+    }
+}
