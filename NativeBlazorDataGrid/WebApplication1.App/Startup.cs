eea678fb43be5237ed790bdbaf7f6f19f1ec4b13
Vladimir Enchev <vladimir.enchev@gmail.com>
2018-12-13 17:22:35 +0200

    app updated

--- NativeBlazorDataGrid/WebApplication1.App/Startup.cs
+++ NativeBlazorDataGrid/WebApplication1.App/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.App.Northwind;
 using WebApplication1.App.Services;
 
 namespace WebApplication1.App
@@ -11,6 +12,9 @@ namespace WebApplication1.App
             // Since Blazor is running on the server, we can use an application service
             // to read the forecast data.
             services.AddSingleton<WeatherForecastService>();
+
+            // Add NorthwindContext
+            services.AddSingleton<NorthwindContext>();
         }
 
         public void Configure(IBlazorApplicationBuilder app)
