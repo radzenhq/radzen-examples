b94eb7abc62691a2214b36d9f667601ca021a6fc
Vladimir Enchev <vladimir.enchev@gmail.com>
2019-02-11 11:18:10 +0200

    Export and apply DataGrid filters app added

---
+++ ExportWithFilter/server/Controllers/ServerMethodsController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExportWithFilter.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ServerMethodsController : Controller
+    {
+        // Sample method which returns the sum of its arguments
+        // For more examples check https://www.radzen.com/documentation/invoke-custom-method/
+        // public IActionResult Sum(int x, int y)
+        // {
+        //    var sum = x + y;
+        //
+        //    return Json(new { sum });
+        // }
+    }
+}
---
+++ ExportWithFilter/server/Models/Sample/Product.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExportWithFilter.Models.Sample
+{
+  [Table("Products", Schema = "dbo")]
+  public partial class Product
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id
+    {
+      get;
+      set;
+    }
+
+
+    [InverseProperty("Product")]
+    public ICollection<OrderDetail> OrderDetails { get; set; }
+    public string ProductName
+    {
+      get;
+      set;
+    }
+    public decimal ProductPrice
+    {
+      get;
+      set;
+    }
+    public string ProductPicture
+    {
+      get;
+      set;
+    }
+  }
+}
