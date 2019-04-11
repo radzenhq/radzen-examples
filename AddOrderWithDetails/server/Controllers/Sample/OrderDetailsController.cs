5d950ae8c2dbe429a89bae7be5bcbf4af81a4ddf
Vladimir Enchev <vladimir.enchev@gmail.com>
2019-04-11 15:18:27 +0300

    AddOrderWithDetails example added

---
+++ AddOrderWithDetails/server/Controllers/Sample/OrderDetailsController.cs
@@ -0,0 +1,196 @@
+using System;
+using System.Net;
+using System.Data;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.AspNet.OData.Query;
+
+
+
+namespace Sample.Controllers.Sample
+{
+  using Models;
+  using Data;
+  using Models.Sample;
+
+  [ODataRoutePrefix("odata/Sample/OrderDetails")]
+  [Route("mvc/odata/Sample/OrderDetails")]
+  public partial class OrderDetailsController : ODataController
+  {
+    private Data.SampleContext context;
+
+    public OrderDetailsController(Data.SampleContext context)
+    {
+      this.context = context;
+    }
+    // GET /odata/Sample/OrderDetails
+    [EnableQuery(MaxExpansionDepth=10,MaxNodeCount=1000)]
+    [HttpGet]
+    public IEnumerable<Models.Sample.OrderDetail> GetOrderDetails()
+    {
+      var items = this.context.OrderDetails.AsQueryable<Models.Sample.OrderDetail>();
+      this.OnOrderDetailsRead(ref items);
+
+      return items;
+    }
+
+    partial void OnOrderDetailsRead(ref IQueryable<Models.Sample.OrderDetail> items);
+
+    [EnableQuery(MaxExpansionDepth=10,MaxNodeCount=1000)]
+    [HttpGet("{Id}")]
+    public SingleResult<OrderDetail> GetOrderDetail(int? key)
+    {
+        var items = this.context.OrderDetails.Where(i=>i.Id == key);
+        return SingleResult.Create(items);
+    }
+    partial void OnOrderDetailDeleted(Models.Sample.OrderDetail item);
+
+    [HttpDelete("{Id}")]
+    public IActionResult DeleteOrderDetail(int? key)
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            var item = this.context.OrderDetails
+                .Where(i => i.Id == key)
+                .FirstOrDefault();
+
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            this.OnOrderDetailDeleted(item);
+            this.context.OrderDetails.Remove(item);
+            this.context.SaveChanges();
+
+            return new NoContentResult();
+        }
+        catch(Exception ex)
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+
+    partial void OnOrderDetailUpdated(Models.Sample.OrderDetail item);
+
+    [HttpPut("{Id}")]
+    [EnableQuery(MaxExpansionDepth=10,MaxNodeCount=1000)]
+    public IActionResult PutOrderDetail(int? key, [FromBody]Models.Sample.OrderDetail newItem)
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (newItem == null || (newItem.Id != key))
+            {
+                return BadRequest();
+            }
+
+            this.OnOrderDetailUpdated(newItem);
+            this.context.OrderDetails.Update(newItem);
+            this.context.SaveChanges();
+
+            var itemToReturn = this.context.OrderDetails.Where(i => i.Id == key);
+            Request.QueryString = Request.QueryString.Add("$expand", "Order,Product");
+            return new ObjectResult(SingleResult.Create(itemToReturn));
+        }
+        catch(Exception ex)
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+
+    [HttpPatch("{Id}")]
+    [EnableQuery(MaxExpansionDepth=10,MaxNodeCount=1000)]
+    public IActionResult PatchOrderDetail(int? key, [FromBody]Delta<Models.Sample.OrderDetail> patch)
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = this.context.OrderDetails.Where(i => i.Id == key).FirstOrDefault();
+
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            patch.Patch(item);
+
+            this.OnOrderDetailUpdated(item);
+            this.context.OrderDetails.Update(item);
+            this.context.SaveChanges();
+
+            var itemToReturn = this.context.OrderDetails.Where(i => i.Id == key);
+            Request.QueryString = Request.QueryString.Add("$expand", "Order,Product");
+            return new ObjectResult(SingleResult.Create(itemToReturn));
+        }
+        catch(Exception ex)
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+
+    partial void OnOrderDetailCreated(Models.Sample.OrderDetail item);
+
+    [HttpPost]
+    [EnableQuery(MaxExpansionDepth=10,MaxNodeCount=1000)]
+    public IActionResult Post([FromBody] Models.Sample.OrderDetail item)
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            this.OnOrderDetailCreated(item);
+            this.context.OrderDetails.Add(item);
+            this.context.SaveChanges();
+
+            var key = item.Id;
+
+            var itemToReturn = this.context.OrderDetails.Where(i => i.Id == key);
+
+            Request.QueryString = Request.QueryString.Add("$expand", "Order,Product");
+
+            return new ObjectResult(SingleResult.Create(itemToReturn))
+            {
+                StatusCode = 201
+            };
+        }
+        catch(Exception ex)
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+  }
+}
---
+++ AddOrderWithDetails/server/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace Sample
+{
+  public class Program
+  {
+      public static void Main(string[] args)
+      {
+          var host = BuildWebHost(args);
+
+          host.Run();
+      }
+
+      public static IWebHost BuildWebHost(string[] args) =>
+          WebHost.CreateDefaultBuilder(args)
+              .UseKestrel()
+              .UseUrls("http://*:5000")
+              .UseStartup<Startup>()
+              .Build();
+  }
+}
